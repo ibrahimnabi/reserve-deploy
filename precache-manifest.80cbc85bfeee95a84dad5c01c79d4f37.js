2433733b31b4cf801eb2a18a941bdf192d9a44c7
alphaverx <alphaverx@gmail.com>
2020-02-08 10:38:46 +0500

    Updates

---
+++ precache-manifest.80cbc85bfeee95a84dad5c01c79d4f37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a9edd2ea09baabccc7e8e09ba0efb4",
+    "url": "/reserve-deploy/index.html"
+  },
+  {
+    "revision": "c71c5bf51cdd1ce1d82d",
+    "url": "/reserve-deploy/static/css/main.d2185bf8.chunk.css"
+  },
+  {
+    "revision": "2322baacd25bccfc7fbf",
+    "url": "/reserve-deploy/static/js/2.53296535.chunk.js"
+  },
+  {
+    "revision": "87851815452100e9aad56fc2b24346e1",
+    "url": "/reserve-deploy/static/js/2.53296535.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c71c5bf51cdd1ce1d82d",
+    "url": "/reserve-deploy/static/js/main.2b2d4263.chunk.js"
+  },
+  {
+    "revision": "8f12bebd267629303bcc",
+    "url": "/reserve-deploy/static/js/runtime-main.ff561872.js"
+  },
+  {
+    "revision": "2d5cad77dc7fc416b2ea5b4a923723bd",
+    "url": "/reserve-deploy/static/media/logo.2d5cad77.mp4"
+  },
+  {
+    "revision": "4361f2b56fee1a4083254176be0a4b0c",
+    "url": "/reserve-deploy/static/media/logo.4361f2b5.png"
+  }
+]);
